f48eda1c3f6a5cb763755d9128bd9571aafc2a25
agarbuno <alfredo.garbuno@itam.mx>
2023-05-29 21:13:42 -0600

    styles the source code

--- .Rprofile
+++ .Rprofile
@@ -4,12 +4,12 @@ if (interactive()) {
 
 options(
   usethis.full_name = "Alfredo Garbuno",
-  usethis.protocol  = "ssh",
+  usethis.protocol = "ssh",
   usethis.description = list(
     "Authors@R" = utils::person(
-        "Alfredo", "Garbuno",
-        email = "alfredo.garbuno@itam.mx",
-        role = c("aut", "cre")
+      "Alfredo", "Garbuno",
+      email = "alfredo.garbuno@itam.mx",
+      role = c("aut", "cre")
     ),
     Version = "0.0.0.9000"
   ),
--- R/continuous.R
+++ R/continuous.R
@@ -1,49 +1,49 @@
-
 #' Gaussian random variable object
 #'
-#' @description 
+#' @description
 #' Normal random variable
-#' 
-#' 
+#'
+#'
 #' @return A [`RandomVariable`] object.
 #'
 
-GaussianRandomVariable <- 
-    R6::R6Class("RandomVariable",
-        cloneable = FALSE,
-          public = list(
-            #' @field mean (`double`)\cr
-            #'  the center of the distribution.
-            mean = NA,
+GaussianRandomVariable <-
+  R6::R6Class("RandomVariable",
+    cloneable = FALSE,
+    public = list(
+      #' @field mean (`double`)\cr
+      #'  the center of the distribution.
+      mean = NA,
 
-            #' @field sd (`positive`)\cr
-            #'  the dispersion around the mean.
-            sd = NA,
+      #' @field sd (`positive`)\cr
+      #'  the dispersion around the mean.
+      sd = NA,
 
-            #' @description
-            #' Initializes the random variable
-            #' 
-            #' @param mean the center of the distribution.
-            #' @param sd   the dispersion around the center of the distribution.
-            initialize = function(mean = 0, sd = 1){
-              self$mean = mean
-              self$sd   = sd
-            },
-            
-            #' @description
-            #' Sampler 
-            #' 
-            #' @param n (integer) the number of random numbers. 
-            sample = function(n = 1){
-              stats::rnorm(n, self$mean, sd = self$sd)
-            },
-            
-            #' @description 
-            #' Density function
-            #' 
-            #' @param x   (array) to evaluate the density function. 
-            #' @param log (Boolean) indicates if we are evaluating the `log-density`. 
-            density = function(x, log = TRUE){
-              stats::dnorm(x, self$mean, sd = self$sd, log = log)
-            }
-          ))
+      #' @description
+      #' Initializes the random variable
+      #'
+      #' @param mean the center of the distribution.
+      #' @param sd   the dispersion around the center of the distribution.
+      initialize = function(mean = 0, sd = 1) {
+        self$mean <- mean
+        self$sd <- sd
+      },
+
+      #' @description
+      #' Sampler
+      #'
+      #' @param n (integer) the number of random numbers.
+      sample = function(n = 1) {
+        stats::rnorm(n, self$mean, sd = self$sd)
+      },
+
+      #' @description
+      #' Density function
+      #'
+      #' @param x   (array) to evaluate the density function.
+      #' @param log (Boolean) indicates if we are evaluating the `log-density`.
+      density = function(x, log = TRUE) {
+        stats::dnorm(x, self$mean, sd = self$sd, log = log)
+      }
+    )
+  )
