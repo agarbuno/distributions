5c5bb489451284a2fbe69bbd6bc905f94e3e452a
agarbuno <alfredo.garbuno@itam.mx>
2023-05-31 10:35:27 -0600

    adds Poisson random variables

--- R/discrete.R
+++ R/discrete.R
@@ -105,3 +105,65 @@ BinomialRandomVariable <-
       }
     )
   )
+
+# Poisson random variables ---------------------------------------------------
+#' PoissonRandomVariable object
+#'
+#' @name PoissonRandomVariable
+#' @family discrete
+#'
+#' @description Defines a Poisson random variable. It uses in the backend
+#'  all functions that defines a [`Poisson`][rpois] random variable. As
+#'  the other definitions in `distributions` this is an
+#'  [`R6`][R6::R6Class] object. Tha parametrization used
+#'  is from a \deqn{X \sim \mathsf{Poisson}(\lambda)\,,}
+#'  where the parameter \eqn{\lambda >0} denotes the rate of ocurrence
+#'  of events.
+#'
+#'  ## Note
+#'   All random variables in `distributions` are defined as [`R6`]
+#'   objects. The `fields` (referenced below) are needed for objects of
+#'   [`R6::R6Class`]. In our context these are what defines
+#'   which specific instance of a
+#'   random variable is used. That is, the public `fields` are the
+#'   parameters of the random variable \eqn{\theta \in \mathbb{R}^p}, for some
+#'   \eqn{p}.
+#'
+#'
+#' @export
+PoissonRandomVariable <-
+  R6::R6Class("Poisson",
+    inherit = DiscreteRandomVariable,
+    cloneable = FALSE,
+    public = list(
+      #' @field lambda (`double`, positive) \cr
+      #'  the number of independent trials, \eqn{\lambda > 0}.
+      lambda = NA,
+
+      #' @description
+      #' Generates a Binomial [`DiscreteRandomVariable`] object with specified
+      #' number of trials (`n`) and probability of success (`theta`).
+      #'
+      #' @param lambda   (`double`, positive) the rate of events.
+      initialize = function(lambda = 1) {
+        self$lambda <- lambda
+      },
+      #' @description
+      #' Generates random variables using the [`stats::rpois()`] function.
+      #'
+      #' @return (`array`) of random numbers from the distribution.
+      #' @template sampler
+      sample = function(nsamples = 1) {
+        stats::rpois(nsamples, self$lambda)
+      },
+
+      #' @description
+      #' Evaluates the density function using the [`stats::dpois()`] function.
+      #'
+      #' @return (`array`) of evaluations at `x`.
+      #' @template density
+      density = function(x, log = TRUE) {
+        stats::dpois(x, self$lambda, log = log)
+      }
+    )
+  )
